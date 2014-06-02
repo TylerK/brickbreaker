fa321e844d5624e1e9c0e26ae0953822c96e3bee
Tyler Kelley <tylerckelley@gmail.com>
2014-06-02 18:58:32 -0400

    holy crap, it almost plays like a real game

--- js/brick-breaker.js
+++ js/brick-breaker.js
@@ -16,7 +16,8 @@ var leftKey         = 37, // Keycode
     rightKey        = 39, // Keycode
     gamesOn         = true, // Todo: Tie this to a start button
     keysDown        = {},
-    grid            = {};
+    grid            = {},
+    destroyed       = [];
 
 
 
@@ -92,42 +93,49 @@ Paddle.prototype.move = function(direction) {
 // ––––––––––––––––––––––––––––––––––––––––––––––
 function Bricks() {
     this.wrapper    = $('#bricks');
-    this.nodes      = {row: '<div class="row"></div>', brick: '<div class="brick"></div>' };
+    this.height     = 0;
+    this.nodes      = {
+                        brick:      '<div class="brick"></div>',
+                        explosion:  '<div class="explosion"></div>',
+                        bits:       '<div class="bits"></div>'
+                    };
     this.brick      = {height: 15, width: 44, margin: 2}; // Not an actual brick object, just the default width\height settings
 }
 
 Bricks.prototype.layoutLevel = function(bricks) {
 
-    var id = 1;
+    var id = 1, y = 0, num = 0;
 
     // Create brick row and columns objects
     for (var i = 0; i < bricks.length; i++) {
 
-        // Row objects, then filling the dom with rows
-        grid[i] = { y: i * this.brick.height };
-        $(this.nodes.row).addClass('row-' + i).appendTo(this.wrapper);
-
         for (var n = 0; n < bricks[i].length; n++) {
 
-            var thisX = n * (this.brick.width + this.brick.margin);
+            num++;
 
             // Column objects, then filling the rows with columns
-            grid[i][n] = {
+            grid[num] = {
                 id: id++,
-                x: thisX,
-                y: 0,
+                x: n * (this.brick.width + 2),
+                y: y,
                 color: bricks[i][n]
             };
 
             $(this.nodes.brick)
-                .attr('id', grid[i][n].id)
-                .css('left', grid[i][n].x)
-                .addClass('brick-type-' + grid[i][n].color)
-                .appendTo('.row-' + i);
+                .attr('id', grid[num].id)
+                .css('left', grid[num].x)
+                .css('top', grid[num].y = y * (this.brick.height + 6))
+                .addClass('brick-type-' + grid[num].color)
+                .appendTo(this.wrapper);
 
         }
 
+        y++;
+        this.height = y * (this.brick.height + 6);
+
     }
+
+    console.log(grid);
 
 };
 
@@ -180,36 +188,38 @@ Bricks.prototype.level = function(level) {
     }
 };
 
-// Todo: Clean this area up
-// Might need to add some logic to make avoid duplicate ID's
 Bricks.prototype.explode = function(brick) {
 
-    var thisBrick       = $(brick),
-        id              = Math.floor(Math.random() * 1000) + 1000,
-        node            = $('<div id="' + id + '" class="confetti"></div>'),
-        confettiAmount  = Math.floor(Math.random() * 15) + 20;
+    var confettiAmount      = 30,
+        explosion           = $(this.nodes.explosion),
+        bits                = this.nodes.bits,
+        explodedBrick       = $('#' + brick.id),
+        explosionColor      = explodedBrick.css('background-color');
 
-    $('body').append(node);
 
-    // Drop in the confetti
-    for (var n = 1; n < confettiAmount; n++) {
-        $(node).append('<div></div>');
+    // Remove that brick!
+    explodedBrick.remove();
+
+    // Drop the explosion on to the dom
+    explosion
+        .attr('id', 'explosion-' + brick.id)
+        .css({
+            top             : brick.y,
+            left            : brick.x,
+            'margin-top'    : '-50px',
+            'margin-left'   : '-50px'
+        })
+        .appendTo(this.wrapper);
+
+    // Drop the exploding bits in to the wrapper
+    for (var n = 0; n < confettiAmount; n++ ) {
+        explosion.append(bits);
     }
 
-    // Position the confetti wrapper
-    $(node).css({
-        top  : parseInt(thisBrick.offset().top) - 40,
-        left : parseInt(thisBrick.offset().left) - 30
-    });
-
-    // Explode out the confetti!
-    $(node).find('div').each(function() {
-
-        // var randomColor = Math.floor(Math.random() * 360);
+    explosion.find('.bits').each(function() {
 
         $(this).css({
-            // 'background-color' : 'hsla(' + randomColor + ', 100%, 50%, 1)'
-            'background-color' : thisBrick.css('background-color')
+            background : explosionColor
         });
 
         $(this).animate({
@@ -225,16 +235,8 @@ Bricks.prototype.explode = function(brick) {
 
     });
 
-    // Kill the confetti parent after couple of seconds
-    setTimeout(function() {
-        $(node).remove();
-    }, 2000);
-
-    thisBrick.addClass('explode');
-
-    score.add(score.amount);
-
 };
+
 
 
 
@@ -286,42 +288,81 @@ Ball.prototype.move = function() {
 
 };
 
-Ball.prototype.collisions = function() {
+// Todo: find better names for these
+Ball.prototype.checkBrickCollisions = function(x, y) {
+
+    var width = bricks.brick.width, height = bricks.brick.height;
+
+    for (var obj in grid) {
+
+        if (destroyed.indexOf(grid[obj].id) < 0) {
+
+            if ( x >= grid[obj].x && x <= grid[obj].x + width && y >= grid[obj].y && y <= grid[obj].y + height) {
+
+                var brick = grid[obj];
+                delete grid[obj];
+
+                destroyed.push(brick);
+                return brick;
+            }
+
+        } else {
+            return false;
+        }
+
+    }
+
+};
+
+Ball.prototype.collisions = function(x, y) {
+
+    var ballX = Math.round(x);
+    var ballY = Math.round(y);
+
 
     // Side wall collision
-    if (this.position.x <= stage.boundary.left + this.width || this.position.x >= stage.boundary.right - this.width) {
+    if (ballX <= stage.boundary.left + this.width || ballX >= stage.boundary.right - this.width) {
         this.bounce('x');
     }
 
     // Top wall
-    else if (this.position.y <= stage.boundary.top + this.width / 2) {
+    else if (ballY <= stage.boundary.top + this.width / 2) {
         this.bounce('y');
     }
 
     // Paddle
     // Todo: Figure out why I need so many arbitrary values
-    else if (this.position.y >= paddle.position.y - this.width - 2 && this.position.x >= paddle.position.x && this.position.x < paddle.position.x + paddle.width) {
+    else if (ballY >= paddle.position.y - this.width - 2 && ballX >= paddle.position.x && ballX < paddle.position.x + paddle.width) {
         this.collide(paddle);
     }
 
     // Below paddle = game over!
-    else if (this.position.y >= paddle.position.y + this.width + 10) {
+    else if (ballY >= paddle.position.y + this.width + 10) {
         console.log('game over');
         gamesOn = false;
     }
 
-    // Check each brick
-    // for (var n = 1; n <= grid.length; n++) {
-    //     console.log(n);
-    // }
 
+    // Did the ball hit a brick?
+    else if(ballY <= bricks.height) {
+
+        var brickHit = this.checkBrickCollisions(ballX, ballY);
+
+        if(brickHit) {
+            console.log('hit brick: ' + brickHit);
+            bricks.explode(brickHit);
+            ball.collide(brickHit);
+            score.add(score.amount);
+            this.bounce('y');
+        }
+    }
 
 };
 
 Ball.prototype.collide = function(elem) {
 
     // console.log('collided with:');
-    // console.log(elem);
+    console.log(elem);
 
     if (elem === paddle) {
 
@@ -338,7 +379,7 @@ Ball.prototype.collide = function(elem) {
 
     }
 
-    else if (elem === brick) {
+    else if (elem.hasOwnProperty('id')) {
 
     }
 
@@ -375,7 +416,7 @@ function Controls() {
 // Scoreboard
 // ––––––––––––––––––––––––––––––––––––––––––––––
 function Score() {
-    this.amount = 1000;
+    this.amount = 1;
     this.wrapper = $('#score');
     this.wrapper.html('0');
 }
@@ -399,6 +440,8 @@ var score     = new Score();
 var controls  = new Controls();
 
 
+
+
 //
 // The game loop
 // ––––––––––––––––––––––––––––––––––––––––––––––
@@ -416,7 +459,7 @@ function gameLoop() {
         }
 
         ball.move();
-        ball.collisions();
+        ball.collisions(ball.position.x, ball.position.y);
     }
 
     setTimeout(gameLoop, 20);
@@ -439,6 +482,6 @@ gameLoop();
 // Testing stuff
 // ––––––––––––––––––––––––––––––––––––––––––––––
 $('.brick').on('click', function() {
-    bricks.explode(this);
+    bricks.explode(this, this.id);
 });
 
